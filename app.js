a526424a95220c7a871d8540421529dcfeeae3a3
Lamasaurus <sandervhove@gmail.com>
2018-05-17 14:32:56 +0200

    Update

--- app.js
+++ app.js
@@ -1,47 +1,14 @@
-"use strict";
-var N3 = require('n3');
-var moment = require('moment');
+"use strict"
+var N3 = require('n3')
+var moment = require('moment')
+const fs = require('fs')
+var spat_log = JSON.parse(fs.readFileSync(__dirname + '/SPAT_log.json', 'utf8')).reverse()
+var index = 0
 
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+function getUpdateForUri(url) {
+  var response = spat_log[(index++) % spat_log.length]
+  response["url"] = url
+  return response
 }
 
-function getRandomCount() {
-  return Math.floor(Math.random() * 100)
-}
-
-function getUpdateForUri(uri) {
-  var now = moment().format();                          // 2018-02-26T14:44:51+01:00
-  // Context is an observation of a crossroad at a certain time
-  var context = 'http://example.org/observations?time=' + now.substring(0,19) // 2018-02-26T14:44:51
-  return {
-    "@context": {
-      "generatedAt": {
-        "@id": "http://www.w3.org/ns/prov#generatedAtTime",
-        "@type": "http://www.w3.org/2001/XMLSchema#date"
-      },
-      "statusLight": "http://example.org/statusLight",
-      "TrafficLight": "http://example.org/TrafficLight",
-      "color": "http://example.org/color",
-      "about": "http://www.w3.org/1999/02/22-rdf-syntax-ns#about",
-      "longitude": "http://www.w3.org/2003/01/geo/wgs84_pos#long",
-      "latitude": "http://www.w3.org/2003/01/geo/wgs84_pos#lat"
-    },
-    "@id": context,
-    "generatedAt": now,
-    "@type": ["http://www.w3.org/ns/prov#Entity", "TrafficLight"],
-    "about": uri,
-    "statusLight": "http://example.org/status#green",
-    "color": getRandomColor(),
-    "count": getRandomCount(),
-  }
-}
-setInterval(function(){ 
-console.log(JSON.stringify(getUpdateForUri("http://data.observer.be/verkeerslichten/1")));
-console.log(JSON.stringify(getUpdateForUri("http://data.observer.be/verkeerslichten/2")));
-}, 3000);
+setInterval(function(){ console.log(JSON.stringify(getUpdateForUri("http://data.observer.be/kruispunten/1"))); }, 200)
